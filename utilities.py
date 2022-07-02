2c190868365fa128013ada2ca1af0b23e126d3e2
A Walton <awalton30@gatech.edu>
2022-07-02 14:34:08 +0100

    updated

--- utilities.py
+++ utilities.py
@@ -5,6 +5,9 @@ utility helper
 """
 
 import plotly.express as px
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
 
 def plot_change_points_pyplot(data_test,dict_sites_melt,file_location_save =None,save_fig=False,show_fig=False):
     for key,data in data_test.items():
@@ -25,3 +28,57 @@ def plot_change_points_pyplot(data_test,dict_sites_melt,file_location_save =None
                         fig.show()
                     if save_fig:
                         fig.write_html(file_location_save+"/{}_plot.html".format(key+'_'+cost))
+
+
+def plot_changepoints(flattened_dict,dict_sites_melt):
+    for key,data in flattened_dict.items():
+        y_sns=dict_sites_melt[key][['datetime','value','variable']]
+        fig,ax=plt.subplots()
+        
+        sns.lineplot(x='datetime',y='value',hue='variable',data=y_sns,ax=ax).set(title=key)
+        
+        for location in data.values():
+            for x in location:
+                x_plot=y_sns['datetime'][x-1]
+                plt.axvline(x_plot,lw=2, color='black',linestyle='--')
+
+def plot_change(x,y,cpd,ax):
+        '''
+    
+    
+        Parameters
+        ----------
+        x : TYPE
+            array of datetime values.
+        y : TYPE
+            array of value pings.
+        cpd : TYPE
+            array of cpd in location (not datetime) format.
+        ax : TYPE
+            axes to plt ont.
+    
+        Returns
+        -------
+        None.
+    
+        '''
+        from itertools import cycle
+        # from ruptures.utils import pairwise
+        from itertools import tee
+
+        n_samples, n_features = np.array(y).reshape(-1,1).shape
+        COLOR_CYCLE = ["#4286f4", "#f44174"]
+
+        color_cycle = cycle(COLOR_CYCLE)
+        # plot s
+        ax.plot(range(n_samples), y)
+
+        # color each (true) regime
+        bkps = [0] + sorted(cpd)
+        alpha = 0.2  # transparency of the colored background
+        #store list as iterators
+        l1,l2=tee(bkps)
+        next(l2,None)
+        zipped_lists=zip(l1,l2)
+        for (start,end ),col in zip(zipped_lists,color_cycle):
+            ax.axvspan(max(0, start - 0.5), end - 0.5, facecolor=col, alpha=alpha)
