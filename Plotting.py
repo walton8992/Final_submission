f338cbcd3a582fc2aeb68020d220d22139d47805
A Walton <awalton30@gatech.edu>
2022-07-03 18:39:28 +0100

    Plotting added loop to remove one line plot

--- Plotting.py
+++ Plotting.py
@@ -7,7 +7,7 @@ load and plot data generate from Pracitcum Class
 #%% library import 
 import utilities
 from utilities import load_data ,delete_folder
-
+import collections
 dict_sites_melt=load_data('Data/site_data_melted')
 binseg_flat_24=load_data('practicum_2022/Results/binseg_flat_24')
 window_flat_24=load_data('practicum_2022/Results/window_flat_24')
@@ -20,9 +20,23 @@ delete_folder('plots/binseg/matplotlib')
 delete_folder('plots/window/pyplot')
 delete_folder('plots/window/matplotlib')
 
+
+#%%
+def remove_unuseful_plots(dictionary:dict):
+    '''
+    Want to remove those with just end plot'''
+   
+    new_dict=collections.defaultdict(dict)
+    plot_dict=dictionary
+    for key,item in plot_dict.items():
+        if len (list(item.values())[0])==1:
+            pass
+        else:
+            new_dict[key] = item
+    return new_dict
 #%%plot
+dict_real=remove_unuseful_plots(window_flat)
 utilities.plot_change_points_pyplot(binseg_flat, dict_sites_melt,file_location_save='plots/binseg/pyplot',save_fig=True)
 utilities.plot_change_points_pyplot(window_flat, dict_sites_melt,file_location_save='plots/window/pyplot',save_fig=True)
-#%%
 utilities.plot_changepoints(binseg_flat,dict_sites_melt,save_loc='plots/binseg/matplotlib')
 utilities.plot_changepoints(window_flat,dict_sites_melt,save_loc='plots/window/matplotlib')
