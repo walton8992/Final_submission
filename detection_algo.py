c345288fa8d5b383595ef503e696a77e33d97cb9
A Walton <awalton30@gatech.edu>
2022-07-16 16:37:09 +0100

    Fixed loading

--- detection_algo.py
+++ detection_algo.py
@@ -5,10 +5,14 @@ Live detection case
 Use multiple thread and process to speed detection
 
 """
-from utilities import load_data
+from utilities import load_data, change_working_dir
 from CPDE_ensemble import changePoint
 
-dict_sites_melt = load_data("Data/site_data_melted")
+change_working_dir(
+    r"C:\Users\Alex\Documents\Georgia Tech Official MSC\Pract_final\practicum_2022"
+)
+dict_sites_melt = load_data(r"Data\melted_dict_data\site_data_melted")
+#%%
 window = changePoint(
     model="window",
     pen=20,
@@ -16,3 +20,4 @@ window = changePoint(
     cost_function=list(["Min_MinAbs"]),
     lookback_duration=True,
 )
+new_sites = window.dict_sites_melt
