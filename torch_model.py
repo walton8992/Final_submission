3ba0707a435b3159308870aa7995143ba5bd5f1f
A Walton <awalton30@gatech.edu>
2022-07-17 21:24:26 +0100

    Final pytorch and writeup

--- torch_model.py
+++ torch_model.py
@@ -5,19 +5,22 @@ pytroh cpde
 """
 
 from TIRE import DenseTIRE as TIRE, utils
-
+import logging
 import torch
 import numpy as np
 import matplotlib.pyplot as plt
 from utilities import load_data, change_working_dir
 import pandas as pd
-from scipy.signal import find_peaks
+from scipy.signal import find_peaks, peak_prominences, peak_widths
 from datetime import timedelta
 import itertools
+from tqdm import tqdm
 
 # change_working_dir("practicum_2022")
-dict_ts = load_data("Data/melted_dict_data/site_data_melted")
 import pathlib
+import time
+
+dict_ts = load_data("Data/melted_dict_data/site_data_melted")
 
 
 def twentyfour_hours(dictionary):
@@ -33,21 +36,19 @@ def twentyfour_hours(dictionary):
     return dictionary
 
 
-dict_ts_twentyfounr = twentyfour_hours(dict_ts)
+dict_ts_twentyfounr = twentyfour_hours(dict_ts.copy())
 save_folder = r"../plots/Autoencoder/"
-small_dict = dict(itertools.islice(dict_ts_twentyfounr.items(), 20))
+small_dict = dict(itertools.islice(dict_ts_twentyfounr.items(), 100))
 
 # %%
-def run_model(site, epoches, title=None):
+def run_model(site, epoches, dict_of_data, title=None, make_folder=False):
     """Run pytorch tire model on site location."""
-    data = dict_ts_twentyfounr[site]
+    data = dict_of_data[site]
     variables = list(data.variable.unique())
     fig, ax = plt.subplots(figsize=(10, 10))
     fig2, ax2 = plt.subplots(figsize=(10, 10))
     peaks = {}
     colours = ["r", "b", "g", "orange"]
-    path = pathlib.Path(save_folder + f"/{site}")
-    path.mkdir(parents=True, exist_ok=True)
 
     for i, var in enumerate(variables):
         data_var = data[data.variable == var].value
@@ -67,8 +68,39 @@ def run_model(site, epoches, title=None):
             color=colours[i],
             label=var,
         )
-        peaks_temp = find_peaks(dissimilarities, width=(150,))[0]
+
+        peaks_temp, peak_values = find_peaks(dissimilarities, width=50)
+        peak_prom = peak_prominences(dissimilarities, peaks_temp)[0]
+
+        # outliers 2 std mean
+        # peak_prom[peak_prom > (peak_mean +( 5*peak_std))]
+
+        # find peaks that are big
+
+        # CHECK  - ony 1 peak
+        if len(peaks_temp) >= 1:
+
+            peak_std = np.std(peak_prom)
+            peak_mean = np.mean(peak_prom)
+            peaks_temp = peaks_temp[peak_prom > (peak_mean + (3 * peak_std))]
+
         peaks[var] = peaks_temp
+
+        time = data[data.variable == var].datetime
+        if len(peaks_temp) > 1:
+            for iloc, location in enumerate(peaks_temp):
+
+                time_loc = time.iloc[location]
+                ax2.axvline(time_loc, lw=2, color=colours[i], linestyle="--")
+        elif len(peaks_temp) == 0:
+            continue
+        else:
+            try:
+                time_loc = time.iloc[peaks_temp]
+                ax2.axvline(time_loc, lw=2, color=colours[i], linestyle="--")
+            except:
+                logging.warning("error in 1d plot")
+                print("cant plot")
     ax.legend()
     ax2.legend()
     if not title:
@@ -79,49 +111,33 @@ def run_model(site, epoches, title=None):
         ax.set_title(title)
     fig.savefig(save_folder + f"/{site}_dissimilarities.png")
     fig2.savefig(save_folder + f"/{site}.png")
-    fig.savefig(str(path) + f"/{site}_dissimilarities.png")
-    fig2.savefig(str(path) + f"/{site}.png")
+    if make_folder:
+        path = pathlib.Path(save_folder + f"/{site}")
+        path.mkdir(parents=True, exist_ok=True)
 
-    # mean_diss = np.mean(dissimilarities)
-    # fig, ax = plt.subplots(figsize=(10, 10))
-    # ax.plot(data_var.datetime, data_var.value)
-    # time = data_var.datetime[19:-20]
-    # from scipy.signal import find_peaks
+        fig.savefig(str(path) + f"/{site}_dissimilarities.png")
+        fig2.savefig(str(path) + f"/{site}.png")
 
-    # min_diss = min(dissimilarities)
-    # max_diss = max(dissimilarities)
-
-    # peaks, _ = find_peaks(
-    #     dissimilarities, prominence=(max_diss / 10, max_diss)
-    # )
-
-    # for iloc, location in enumerate(peaks):
-
-    #     time_loc = time.iloc[location]
-    #     plt.axvline(time_loc, lw=2, color="black", linestyle="--")
     return peaks
 
 
-# %%
-cuba = run_model("Cuba_1900", 1)
-# %%
-ely_east = run_model("Ely East-10619", 1)
 #%%
-moore = run_model("Moore Ranch", 1)
-# %%
-moore = run_model("Devil's Head-203", 1)
+if __name__ == "__main__":
+    dict_results = {}
+    start_time = time.time()
 
-#%%
-dict_results = {}
-for mbile_site in small_dict:
-    dict_results[mbile_site] = run_model(mbile_site, 1)
-dict_results["ely"] = run_model("Ely East-10619", 1)
-# %%
-dict_cost_norm = dict(
-    (x, s)
-    for x, y in dict_results.items()
-    for r, s in y.items()
-    if len(s) != 0
-)
-# get plots in
-# HELP increase epcoch increases sens
+    for mbile_site in small_dict:
+        dict_results[mbile_site] = run_model(mbile_site, 1, small_dict)
+    end_time = time.time()
+    total_time = end_time - start_time
+
+    # %%
+    # %%
+    dict_empty = {}
+    for x, y in dict_results.items():
+        for a, b in y.items():
+            if len(b) != 0:
+                if x not in dict_empty:
+                    dict_empty[x] = [list(b)]
+                else:
+                    dict_empty[x].append(list(b))
