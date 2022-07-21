8e7169fc2a64329a94b18cd43fda93de2574f452
walton8992 <a.m.walton@outlook.com>
2022-07-21 07:21:05 +0100

    Delete Submission directory

--- Submission/ruptures/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Offline change point detection for Python."""
-
-from .datasets import pw_constant, pw_linear, pw_normal, pw_wavy
-from .detection import Binseg, BottomUp, Dynp, KernelCPD, Pelt, Window,WindowEnsemble,BinsegEnsemble
-from .exceptions import NotEnoughPoints
-from .show import display
-# Convenient access to the version number
-from .version import version as __version__
